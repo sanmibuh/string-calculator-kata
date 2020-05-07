22ee86770d5714a0e4c9b75dd3691dc67396bf57
David Belmez Vicente <sanmibuh@gmail.com>
2020-05-07 16:54:59 +0200

    [20200507]: Refactor

--- src/main/java/com/sanmibuh/kata/stringcalculator/StringCalculator.java
+++ src/main/java/com/sanmibuh/kata/stringcalculator/StringCalculator.java
@@ -4,11 +4,17 @@ import java.util.stream.Stream;
 
 public final class StringCalculator {
 
+  private static final String SEPARATOR = ",";
+
+  private StringCalculator() {
+    //util class
+  }
+
   public static int add(final String input) {
     if (input.isEmpty()) {
       return 0;
     }
-    return Stream.of(input.split(","))
+    return Stream.of(input.split(SEPARATOR))
         .mapToInt(Integer::parseInt)
         .sum();
   }
